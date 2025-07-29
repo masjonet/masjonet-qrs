30463217b391aa085cc358028d84768e412b4e54
ajbinarto <ajbinarto@gmail.com>
2025-07-29 14:18:36 +0700

    first commit

---
+++ src/main/java/id/co/jonet/springboot_mycqrs_commnd/SpringbootMycqrsCommndApplication.java
@@ -0,0 +1,13 @@
+package id.co.jonet.springboot_mycqrs_commnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMycqrsCommndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMycqrsCommndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/id/co/jonet/springboot_mycqrs_commnd/SpringbootMycqrsCommndApplicationTests.java
@@ -0,0 +1,13 @@
+package id.co.jonet.springboot_mycqrs_commnd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMycqrsCommndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
